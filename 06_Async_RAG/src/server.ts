e717cc1bd82a254be694bf60128fedd4f5b4db24
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2026-08-25 21:55:14 +0530

    feat: implement server entry point and integrate RagService for document processing

--- 06_Async_RAG/src/server.ts
+++ 06_Async_RAG/src/server.ts
@@ -1,11 +1,20 @@
 import { buildApp } from "./app.js"
+import { RagService } from "./Rag/Services/RagService.js";
 
 const start = async () => {
     const app = await buildApp();
+    const ragService = new RagService();
+
     try {
-        app.listen(({ port: 3000 }), async () => {
-            app.log.info(`server started`)
-        })
+        await app.listen({ port: 3000 });
+
+        const filePath = "C:/Users/alkar/Downloads/Print _ Udyam Registration Certificate.pdf";
+        const query = "what is this pdf about?"
+
+        await ragService.setupDb(filePath);
+        const ans = await ragService.askQuestion(query);
+        app.log.info(ans);
+
     } catch (err) {
         app.log.error(err);
     }
